4c5caa06f531425c867b965685dab50795b253ca
Lepilkin Aleksandr <heist2000@gmail.com>
2023-12-28 11:54:44 +0300

    made convert to CSV

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # StickyPassword_to_CSV
+This programm convert Sticky Password database txt format to CSV format by KeePassXC
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
-import json
-from datetime import datetime
-
-FILE = "SP_UZI.txt"
-STR_START_ACCAUNT = 'Имя учетной записи:  '
+import csv
 
 
 def make_dict(data: list[str]) -> dict:
+    """Функция конвертирует монопользовательскую учетную запись формата 
+    StickyPassword в формат KeePassXC
+
+    Args:
+        data (list[str]): список записей для одного пользователя в формате StickyPassword
+
+    Returns:
+        dict: словарь записей для одного пользователя в формате KeePassXC
+    """
     tmp = {i.split(': ')[0]: i.split(': ')[-1].strip() for i in data}
     
     res = {"Group": "Корень/Учетные записи интернета"}
@@ -19,29 +24,70 @@ def make_dict(data: list[str]) -> dict:
     return res
     
 def make_list_of_dicts(data: list[str]) -> list[dict]:
-    pass
+    """Функция конвертирует многопользовательскую учетную запись из формата
+    StickyPassword в формат KeePassXC
 
-if __name__ == "__main__":
-    with open(FILE, 'r', encoding='utf-16') as file:
-        count = 0
-        res = list()
+    Args:
+        data (list[str]): список записей для нескольких пользователей в 
+        формате StickyPassword
+
+    Returns:
+        list[dict]: список словарей с записями пользователей в формате KeePassXC
+    """
+    res = list()
+    tmp = list()
+    first_two_string = data[:2]
+    for i in data[3:]:
+        if i.startswith("Имя пользователя:") and tmp:
+            res.append(make_dict(tmp))
+            tmp = first_two_string[:]
+        elif i.startswith("Имя пользователя:"):
+            tmp = first_two_string[:]
+        tmp.append(i)
+    res.append(make_dict(tmp))
+    return res
+
+def make_json_from_txt(filename: str) -> None:
+    """Функция построчно читает файл БД StickyPassword в формате txt
+    и конвертирует данные в список словарей, каждый словарь в формате KeePassXC
+
+    Args:
+        filename (str): путь к txt файлу БД StickyPassword
+        
+    Returns:
+        list[dict]: список словарей с данными аккаунтов
+    """
+    with open(filename, 'r', encoding='utf-16') as file:
+        data_accounts = list()
         for line in file:
             line = line.strip()
-            if count == 3:
-                break
-            if line.startswith(STR_START_ACCAUNT):
-                #print(line.split(STR_START_ACCAUNT)[-1].strip())
+            if line.startswith('Имя учетной записи:'):
                 tmp = list()
                 while line != '\n':
-                    #tmp[line.split(': ')[0]] = line.split(': ')[-1].strip()
-                    tmp.append(line)
+                    tmp.append(line.strip())
                     line = file.readline()
                 if any(i.startswith("Имен пользователя:") for i in tmp):
-                    print('!!!!!!!!!!!!!')
+                    data_accounts += make_list_of_dicts(tmp)
                 else:
-                    res.append(make_dict(tmp))
+                    data_accounts.append(make_dict(tmp))
                     
-            #count += 1
-    with open('data.json', 'w', encoding='utf-8') as file:
-        json.dump(res, file, indent=4, ensure_ascii=False)            
-    #print(res)
+    #with open('data.json', 'w', encoding='utf-8') as file:
+    #    json.dump(data_accounts, file, indent=4, ensure_ascii=False)    
+        
+    return data_accounts        
+
+def make_csv(to_csv: list[dict]) -> None:
+    """Функция конвертирует список словарей в CSV и сохраняет в sticky_db.csv
+
+    Args:
+        to_csv (list[dict]): список словарей с данными аккаунтов
+    """
+    keys = to_csv[0].keys()
+    with open('sticky_db.csv', 'w', newline='', encoding='utf-8') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(to_csv)
+
+if __name__ == "__main__":
+    data = make_json_from_txt(filename="SP_UZI.txt")
+    make_csv(data)
